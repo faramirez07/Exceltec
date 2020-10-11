9aed6c2e45498de278a33e0630c3b521fb7d7c32
framirez07 <faramirez07@hotmail.com>
2020-10-11 17:39:52 -0600

    Cambio
    Modificación de código documentado

--- Exceltec/Controllers/HomeController.cs
+++ Exceltec/Controllers/HomeController.cs
@@ -26,21 +26,28 @@ namespace Exceltec.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Método para ingresar un nuevo registro de mascota
+        /// </summary>
+        /// <param name="mascotaPar"></param>
+        /// <returns></returns>
+
         [HttpPost]
         public ActionResult Nuevo(Masco mascotaPar)
         {
             EntidadMascota mascota = new EntidadMascota();
             List<EntidadMascota> ListMascota = new List<EntidadMascota>();
+            //Referencia al WEBAPI proporcionado de swagger
             HttpClient clientHttp = new HttpClient();
             clientHttp.BaseAddress = new Uri("https://petstore.swagger.io/v2/");
 
             var request = clientHttp.PostAsync("pet", mascotaPar, new JsonMediaTypeFormatter()).Result;
-
+            //Al ser positivo la conexión entra a ejecutar la consulta.
             if (request.IsSuccessStatusCode)
             {
                 var resulstring = request.Content.ReadAsStringAsync().Result;
                 var resutado = resulstring.Count();
-                if (resutado>0)
+                if (resutado > 0)
                 {
                     return RedirectToAction("About");
                 }
@@ -51,21 +58,26 @@ namespace Exceltec.Controllers
             return View(mascotaPar);
         }
 
+        /// <summary>
+        /// Méteodo para obtener una máscota en especifico y mostrarla
+        /// </summary>
+        /// <param name="filtro"></param>
+        /// <returns></returns>
         public ActionResult About(string filtro)
         {
+            //Referencia al WEBAPI proporcionado de swagger
             HttpClient clientHttp = new HttpClient();
             clientHttp.BaseAddress = new Uri("https://petstore.swagger.io/v2/pet/" + filtro);
 
             var request = clientHttp.GetAsync("").Result;
             EntidadMascota mascota = new EntidadMascota();
             List<EntidadMascota> ListMascota = new List<EntidadMascota>();
+            //Al ser positivo la conexión entra a ejecutar la consulta.
             if (request.IsSuccessStatusCode)
             {
                 var resulstring = request.Content.ReadAsStringAsync().Result;
-                //var listado = JsonConvert.DeserializeObject(resulstring);
-                // List<string> Customer = new JavaScriptSerializer().Deserialize<List<string>>(resulstring);
-                //var numero = listado[0]["name"].ToString();
                 var personaIntermedioObject = JObject.Parse(resulstring);
+                //Se cambia a otra entidad para que tenga todas las variables del json
                 mascota.Nombre = personaIntermedioObject["name"].ToString();
                 mascota.Id = Convert.ToInt32(personaIntermedioObject["id"]);
                 mascota.Disponible = personaIntermedioObject["status"].ToString();
@@ -73,13 +85,16 @@ namespace Exceltec.Controllers
                 {
                     ListMascota.Add(mascota);
                 }
-               
+
 
                 return View(ListMascota);
             }
-                return View(new List<EntidadMascota>());
+            return View(new List<EntidadMascota>());
         }
-
+        /// <summary>
+        /// Método para la pagina de contacto esta desabilitada
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -87,29 +102,31 @@ namespace Exceltec.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Método para obtener el valor a actualizar de un registro de mascota
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
         [HttpGet]
         public ActionResult Actualizar(int id)
         {
+            //Referencia al WEBAPI proporcionado de swagger
             HttpClient clientHttp = new HttpClient();
             clientHttp.BaseAddress = new Uri("https://petstore.swagger.io/v2/pet/" + id);
 
             var request = clientHttp.GetAsync("").Result;
             EntidadMascota mascota = new EntidadMascota();
             List<EntidadMascota> ListMascota = new List<EntidadMascota>();
+            //Al ser positivo la conexión entra a ejecutar la consulta.
             if (request.IsSuccessStatusCode)
             {
                 var resulstring = request.Content.ReadAsStringAsync().Result;
-                //var listado = JsonConvert.DeserializeObject(resulstring);
-                // List<string> Customer = new JavaScriptSerializer().Deserialize<List<string>>(resulstring);
-                //var numero = listado[0]["name"].ToString();
                 var personaIntermedioObject = JObject.Parse(resulstring);
+                //Se cambia a otra entidad para que tenga todas las variables del json
                 mascota.Nombre = personaIntermedioObject["name"].ToString();
                 mascota.Id = Convert.ToInt32(personaIntermedioObject["id"]);
                 mascota.Disponible = personaIntermedioObject["status"].ToString();
-                //foreach (var item in personaIntermedioObject.First)
-                //{
-                //    ListMascota.Add(mascota);
-                //}
 
 
                 return View(mascota);
@@ -117,9 +134,15 @@ namespace Exceltec.Controllers
             return View(new EntidadMascota());
         }
 
+        /// <summary>
+        /// Método para actualizar un valor especifico de mascota
+        /// </summary>
+        /// <param name="mascotaPar"></param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Actualizar(EntidadMascota mascotaPar)
         {
+            //Referencia al WEBAPI proporcionado de swagger
             HttpClient clientHttp = new HttpClient();
             clientHttp.BaseAddress = new Uri("https://petstore.swagger.io/v2/");
             Masco par = new Masco();
@@ -127,7 +150,7 @@ namespace Exceltec.Controllers
             par.name = mascotaPar.Nombre;
             par.status = mascotaPar.Disponible;
             var request = clientHttp.PostAsync("pet", par, new JsonMediaTypeFormatter()).Result;
-
+            //Al ser positivo la conexión entra a ejecutar la consulta.
             if (request.IsSuccessStatusCode)
             {
                 var resulstring = request.Content.ReadAsStringAsync().Result;
@@ -141,14 +164,21 @@ namespace Exceltec.Controllers
             return View(mascotaPar);
         }
 
+        /// <summary>
+        /// Método para eliminar un registro especifico de mascota
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
         [HttpGet]
         public ActionResult Eliminar(int id)
         {
+            //Referencia al WEBAPI proporcionado de swagger
             HttpClient clientHttp = new HttpClient();
             clientHttp.BaseAddress = new Uri("https://petstore.swagger.io/v2/");
 
             var request = clientHttp.DeleteAsync("pet/" + id).Result;
-
+            //Al ser positivo la conexión entra a ejecutar la consulta.
             if (request.IsSuccessStatusCode)
             {
                 var resulstring = request.Content.ReadAsStringAsync().Result;
